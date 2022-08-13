4363c16243104e23a947cca4855e983caacb8d82
TheEyes <fadali_1992@hotmail.com>
2022-08-13 16:14:08 +0100

    attr

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-NAME	=	
+NAME	=	containers
 SRC		=	.cpp
 CPP		=	c++
 FLAGS	=	-Wall -Werror -Wextra -std=c++98
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -1,31 +1,62 @@
 #pragma once
 
+#include <vector>
+
 // #include <memory>
 
 namespace ft {
     template < class T, class Alloc = allocator<T> >
     class vector{
 		public:
-        	typedef		T	value_type; // 1st template parameter T
+        	typedef		T												value_type; // 1st template parameter T
 			typedef		typename	Alloc								allocator_type;
 			typedef		typename	allocator_type::reference			reference;
 			typedef		typename	allocator_type::const_reference		const_reference;
 			typedef		typename	allocator_type::pointer				pointer;
 			typedef 	typename	allocator_type::const_pointer 		const_pointer;
+			typedef		typename	std::vector<T>::iterator			iterator;
+			typedef		typename	std::vector<T>::const_iterator		const_iterator;
+			typedef		typename	std::vector<T>::reverse_iterator	reverse_iterator;
+			typedef		typename	std::vector<T>::const_reverse_iterator	const_reverse_iterator;
+			typedef 	typename	allocator_type::difference_type 		difference_type;
+			typedef 				size_t								size_type;
 
 		private:
-
+			allocator_type		_alloc;
+			value_type			*_container;
+			size_type			_size;
+			size_type			_capacity;
 
 		public:
-			explicit vector (const allocator_type& alloc = allocator_type()){};
-			explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()) {};
+			explicit vector (const allocator_type& alloc = allocator_type()) : _alloc(alloc) *_container(NULL) _size(0) _capacity(0) {};
+
+			explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()) : _alloc(alloc) *_container(NULL) _size(n) _capacity(n) {
+
+			};
 			template <class InputIterator>
          	vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()) {};
 
-			vector (const vector& x) {};
+			vector (const vector &x) {
+				
+			};
 
 			vector& operator= (const vector& x) {};
 
 			~vector(){};
+
+			iterator begin(){
+				return _arr;
+			}
+
+			const_iterator begin() const {
+				return _const_arr;
+			}
+
+			iterator end(){
+				return 
+			};
+			const_iterator end() const{
+
+			};
     };
 };
