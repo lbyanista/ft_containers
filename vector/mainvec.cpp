d6c9ff3e79bb6aa6165b79d2a65dbf10248ef018
TheEyes <fadali_1992@hotmail.com>
2022-08-16 17:00:31 +0100

    somshiit

---
+++ vector/mainvec.cpp
@@ -0,0 +1,12 @@
+#include "vector.hpp"
+#include <iostream>
+int main(){
+
+    int a = 1;
+    ft::vector<int> v(a, a);
+    // ft::vector<int> v(10, a);
+    // ft::vector<int> v(10, int(a));
+    // delete a;
+
+    return 0;
+}
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -2,14 +2,14 @@
 
 #include <vector>
 
-// #include <memory>
+#include <memory>
 
 namespace ft {
-    template < class T, class Alloc = allocator<T> >
+    template < class T, class Alloc = std::allocator<T> >
     class vector{
 		public:
         	typedef		T												value_type; // 1st template parameter T
-			typedef		typename	Alloc								allocator_type;
+			typedef		Alloc											allocator_type;
 			typedef		typename	allocator_type::reference			reference;
 			typedef		typename	allocator_type::const_reference		const_reference;
 			typedef		typename	allocator_type::pointer				pointer;
@@ -19,11 +19,11 @@ namespace ft {
 			typedef		typename	std::vector<T>::reverse_iterator	reverse_iterator;
 			typedef		typename	std::vector<T>::const_reverse_iterator	const_reverse_iterator;
 			typedef 	typename	allocator_type::difference_type 		difference_type;
-			typedef 				size_t								size_type;
+			typedef 				size_t									size_type;
 
 		private:
 			allocator_type		_alloc;
-			value_type			*_container;
+			pointer				_container;
 			size_type			_size;
 			size_type			_capacity;
 
@@ -31,38 +31,50 @@ namespace ft {
 			explicit vector (const allocator_type& alloc = allocator_type()) : _alloc(alloc), _container(NULL), _size(0), _capacity(0) {};
 
 			explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()) : _alloc(alloc), _container(NULL), _size(n), _capacity(n) {
+				this->_container = this->_alloc.allocate(this->_size);
+				for (size_type i = 0; i < this->_size; i++)
+				{
+					// this->_container[i] = val;
 
+					// *(pointer + i)
+					// pointer[i]
+
+					this->_alloc.construct(this->_container + i, val);
+				}
+				
 			};
-			template <class InputIterator>
-         	vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()) {};
+			// template <class InputIterator>
+         	// vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()) {
+
+			// };
 
 			vector (const vector &x) {
 				*this = x;
 			};
 
 			vector& operator= (const vector& x) {
-				this._alloc = x._alloc;
-				this._container = x._container;
-				this._size = x.size;
-				this._capacity = x._capacity;
+				this->_alloc = x._alloc;
+				this->_container = x._container;
+				this->_size = x.size;
+				this->_capacity = x._capacity;
 				return (*this);
 			};
 
 			~vector(){};
 
-			iterator begin(){
-				return _arr[0];
-			}
+			// iterator begin(){
+			// 	return _arr[0];
+			// }
 
-			const_iterator begin() const {
-				return _const_arr;
-			}
+			// const_iterator begin() const {
+			// 	return _arr;
+			// }
 
-			iterator end(){
-				return _arr[size];
-			};
-			const_iterator end() const{
-				return _arr[size];
-			};
+			// iterator end(){
+			// 	return _arr[size];
+			// };
+			// const_iterator end() const{
+			// 	return _arr[size];
+			// };
     };
 };
